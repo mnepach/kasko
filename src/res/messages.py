1993dbcebca8f17d1c0ab55a40bd58e7c40e97c5
mnepach <mnepach@gmail.com>
2025-05-17 00:03:44 +0300

    omly - only fixed messages.py

--- src/res/messages.py
+++ src/res/messages.py
@@ -2,7 +2,7 @@
 
 VEHICLE_YEAR_PROMPT = "Enter the vehicle year of production"
 VEHICLE_PRICE_PROMPT = "Enter the vehicle price"
-TERRITORY_PROMPT = "Omly Belarus (Y/N)?"
+TERRITORY_PROMPT = "Only Belarus (Y/N)?"
 IS_GEELY_PROMPT = "Car model is GEELY (Y/N)?"
 IS_BMW_PROMPT = "Car model is BMW (Y/N)?"
 DRIVER_AGE_PROMPT = "Enter driver age"
